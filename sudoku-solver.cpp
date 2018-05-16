5c1e695a7fb9c4b910f7cc23bc55f232dbff13ec
sohnryang <loop.infinitely@gmail.com>
2018-05-16 23:10:16 +0000

    coded buggy solver

--- sudoku-solver.cpp
+++ sudoku-solver.cpp
@@ -2,7 +2,7 @@
  
 using std::cin;
 using std::cout;
- 
+
 unsigned int board[9][9];
 unsigned int cand[9][9];
  
@@ -11,7 +11,7 @@ void read_input() {
         for (int x = 0; x < 9; ++x)
             cin >> board[y][x];
 }
- 
+
 void generate_candidates() {
 	// horizontal
     for (int y = 0; y < 9; ++y) {
@@ -20,10 +20,11 @@ void generate_candidates() {
             if (board[y][x] != 0)
                 mask &= 1 << (board[y][x] - 1);
         for (int x = 0; x < 9; ++x) {
-            if (board[y][x] == 0) cand[y][x] = mask;
+            if (board[y][x] == 0) cand[y][x] &= (~mask);
             else cand[y][x] = 0;
+        }
     }
- 
+
     // vertical
     for (int x = 0; x < 9; ++x) {
         int mask = (1 << 9) - 1; // 0b111111111
@@ -31,19 +32,49 @@ void generate_candidates() {
             if (board[y][x] != 0)
                 mask &= 1 << (board[y][x] - 1);
         for (int y = 0; y < 9; ++y) {
-            if (board[y][x] == 0) cand[y][x] = mask;
+            if (board[y][x] == 0) cand[y][x] &= (~mask);
             else cand[y][x] = 0;
-    }
- 
-    // boxes
-    for (int i = 0; i < 9; ++i) {
-        int mask = (1 << 9) - 1; // 0b111111111
-        for (int j = 0; j < 9; ++j) {
- 
         }
     }
+
+    // boxes
+    const int box_position[9][2] = {
+        {0, 0}, {3, 0}, {6, 0},
+        {3, 0}, {3, 3}, {3, 6},
+        {6, 0}, {6, 3}, {6, 6}
+    };
+    for (int i = 0; i < 9; ++i) {
+        int mask = (1 << 9) - 1; // 0b111111111
+        int pos[2] = {box_position[i][0], box_position[i][0]};
+        for (int j = pos[0]; j < pos[0] + 3; ++j) {
+            for (int k = pos[1]; k < pos[1] + 3; ++k)
+                if (board[j][k] != 0)
+                    mask &= 1 << (board[j][k] - 1);
+            for (int k = pos[1]; k < pos[1] + 3; ++k) {
+				if (board[j][k] == 0) cand[j][k] &= (~mask);
+				else cand[j][k] = 0;
+			}
+		}
+    }
 }
- 
+
+bool check_solved() {
+	for (int y = 0; y < 9; y++)
+		for (int x = 0; x < 9; x++)
+			if (board[y][x] == 0) return false;
+	return true;
+}
+
+void iterative_solve() {
+	while (!check_solved()) {
+		for (int y = 0; y < 9; y++)
+			for (int x = 0; x < 9; x++)
+				if (__builtin_popcount(cand[y][x]) == 0)
+					board[y][x] = __builtin_ctz(cand[y][x]) + 1;
+		generate_candidates();
+	}
+}
+
 void print_result() {
     for (int y = 0; y < 9; ++y) {
         for (int x = 0; x < 9; ++x)
@@ -51,9 +82,11 @@ void print_result() {
         cout << '\n';
     }
 }
- 
+
 int main() {
 	read_input();
+	generate_candidates();
+	iterative_solve();
 	print_result();
     return 0;
 }
