17ad5ee109cd87e240fd81bfda14668bcbef7b1e
sohnryang <loop.infinitely@gmail.com>
2018-06-12 22:56:44 +0900

    finally finished solver.js

--- solver.js
+++ solver.js
@@ -1,7 +1,7 @@
 function cross(A, B) {
     let result = [];
-    for (a of A) {
-        for (b of B) {
+    for (let a of A) {
+        for (let b of B) {
             result.push(a + b);
         }
     }
@@ -16,24 +16,24 @@ let squares = cross(rows, cols);
 let unitlist = [...cols.map(c => cross(rows, c)),
                 ...rows.map(r => cross(r, cols))];
 let tmp = [];
-for (rs of ['ABC'.split(''), 'DEF'.split(''), 'GHI'.split('')]) {
-    for (cs of ['123'.split(''), '456'.split(''), '789'.split('')]) {
+for (let rs of ['ABC'.split(''), 'DEF'.split(''), 'GHI'.split('')]) {
+    for (let cs of ['123'.split(''), '456'.split(''), '789'.split('')]) {
         tmp.push(cross(rs, cs));
     }
 }
 unitlist.push(...tmp);
 
 let units = {};
-for (s of squares) {
+for (let s of squares) {
     let tmp = [];
-    for (u of unitlist) {
+    for (let u of unitlist) {
         if (u.includes(s)) tmp.push(u);
     }
     units[s] = tmp;
 }
 
 let peers = {};
-for (s of squares) {
+for (let s of squares) {
     let a = new Set([].concat.apply([], units[s]));
     let b = new Set([s]);
     peers[s] = new Set([...a].filter(x => !b.has(x)));
@@ -46,11 +46,11 @@ function eliminate(values, s, d) {
     else if (values[s].length == 1) {
         let d2 = values[s][0];
         let available = true;
-        for (s2 of peers[s]) available = available && eliminate(values, s2, d2) !== false;
+        for (let s2 of peers[s]) available = available && eliminate(values, s2, d2) !== false;
         if (!available) return false;
     }
 
-    for (u of units[s]) {
+    for (let u of units[s]) {
         let dplaces = u.filter(s => values[s].includes(d));
         if (dplaces.length == 0) return false;
         else if (dplaces.length == 1)
@@ -63,7 +63,7 @@ function eliminate(values, s, d) {
 function assign(values, s, d) {
     let other_values = values[s].join('').replace(d, '').split('');
     let available = true;
-    for (d2 of other_values) {
+    for (let d2 of other_values) {
         available = available && eliminate(values, s, d2) !== false;
     }
     return available ? values : false;
@@ -79,10 +79,10 @@ function grid_values(grid) {
 
 function parse_grid(grid) {
     let values = {};
-    for (s of squares) {
+    for (let s of squares) {
         values[s] = digits;
     }
-    for (value of Object.entries(grid_values(grid))) {
+    for (let value of Object.entries(grid_values(grid))) {
         let [s, d] = value;
         if (digits.includes(d) && assign(values, s, d) === false)
             return false;
@@ -95,7 +95,7 @@ function solve(grid) {
 }
 
 function some(seq) {
-    for (e of seq)
+    for (let e of seq)
         if (e) return e;
     return false;
 }
@@ -103,19 +103,19 @@ function some(seq) {
 function search(values) {
     if (values == false) return false;
     let all_len_is_1 = true;
-    for (s of squares) {
-        all_len_is_1 = all_len_is_1 && values[s] == 1;
+    for (let s of squares) {
+        all_len_is_1 = all_len_is_1 && values[s].length == 1;
     }
     if (all_len_is_1) return values;
     let n = 9, s;
-    for (sq of squares) {
+    for (let sq of squares) {
         if (values[sq].length < n && values[sq].length > 1) {
             n = values[sq];
             s = sq;
         }
     }
     let result = [];
-    for (d of values[s]) {
+    for (let d of values[s]) {
         result.push(search(assign(Object.assign({}, values), s, d)));
     }
     return some(result);
@@ -125,5 +125,5 @@ function display(values) {
     console.log(values);
 }
 
-let grid1 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'.split('');
-display(parse_grid(grid1));
+let grid1 = '...92......68.3...19..7...623..4.1....1...7....8.3..297...8..91...5.72......64...'.split('');
+display(solve(grid1));
